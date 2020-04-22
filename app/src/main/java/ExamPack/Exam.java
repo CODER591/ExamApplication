26c86cd149d5482bde8a442dd40bf330a4b04dd3
Sviatoslav Dubei <sdube@tokyo.softservecom.com>
2020-04-22 14:24:06 +0300

    initial commit

---
+++ app/src/main/java/ExamPack/Exam.java
@@ -0,0 +1,22 @@
+package ExamPack;
+
+import ComposeExamDB.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exam {
+        private String topic;
+
+        Exam(List<String>path_to_json_files_with_tests) {
+                //read one file -> construct one test
+                //re
+
+        }
+
+}
+
+/*
+*   Exam(GetDB("path","json").getDB());
+*
+*
+* */
