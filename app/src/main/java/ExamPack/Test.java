0be9a25474599754c84619d07cc480a4b631263a
CODER591 <coder591@loves_you.com>
2020-04-23 00:07:19 +0300

    removed comments

--- app/src/main/java/ExamPack/Test.java
+++ app/src/main/java/ExamPack/Test.java
@@ -62,7 +62,8 @@ public class Test {
 
    public Test(JSONObject data) throws JSONException {
        questions = new ArrayList<>();
-       String tring = data.getString("name");
+       this.test_name = data.getString("name");
+       this.description =data.getString("description");
        JSONArray questionsArray = data.getJSONArray("questions");
        for (int i = 0; i < questionsArray.length(); i++) {
            //JSONArray consist of JSONObjects;
--- app/src/main/java/com/example/testsapplication/MainActivity.java
+++ app/src/main/java/com/example/testsapplication/MainActivity.java
@@ -23,33 +23,10 @@ public class MainActivity extends AppCompatActivity {
 
 
         TextView vieeew = findViewById(R.id.json_disp);
-
         String result="{\"name\":\"Test1\",\"description\":\"Cool and inspiring description\",\"questions\":[{\"question\":\"some question first\",\"answers\":[{\"answer\":\"first_answer\",\"is_correct\":\"false\"},{\"answer\":\"second_answer\",\"is_correct\":\"false\"},{\"answer\":\"third_answer\",\"is_correct\":\"true\"},{\"answer\":\"forth_answer\",\"is_correct\":\"false\"}]},{\"question\":\"some question second\",\"answers\":[{\"answer\":\"first_answer\",\"is_correct\":\"false\"},{\"answer\":\"second_answer\",\"is_correct\":\"true\"},{\"answer\":\"third_answer\",\"is_correct\":\"false\"},{\"answer\":\"forth_answer\",\"is_correct\":\"false\"}]}]}";
         try {
-
             JSONObject jObject = new JSONObject(result);
-            /* Parsing particular Test*/
-/*          String tring = jObject.getString("name");
-            JSONArray questionsArray = jObject.getJSONArray("questions");
-            for(int i=0;i < questionsArray.length(); i++ ){
-                //JSONArray consist of JSONObjects;
-                //One iteration is one question
-                JSONObject question = questionsArray.getJSONObject(i); //parsing the
-                String question_body = question.getString("question");
-
-                JSONArray answersArray = question.getJSONArray("answers");
-                //One iteration is one answer
-                for(int k=0;k<answersArray.length();k++) {
-                    JSONObject answer = answersArray.getJSONObject(k);
-                    String answer_body = answer.getString("answer");
-                    boolean correct = answer.getBoolean("is_correct");
-                }
-
-
-            }
-            vieeew.setText(tring);
-            */
-           Test tst =new  Test(jObject);
+            Test tst =new  Test(jObject);
             vieeew.setText("SEE the might ?");
         } catch (JSONException e) {
             vieeew.setText("ASS we can");
