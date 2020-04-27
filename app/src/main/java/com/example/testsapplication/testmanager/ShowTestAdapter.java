ed770cc7021f7dbc20c4c7e0e2b6016f0fcea350
CODER591 <coder591@loves_you.com>
2020-04-27 13:17:22 +0300

    optimisied code and added clicker to RecyclerView item

--- app/src/main/java/com/example/testsapplication/testmanager/ShowTestAdapter.java
+++ app/src/main/java/com/example/testsapplication/testmanager/ShowTestAdapter.java
@@ -29,11 +29,9 @@ public class ShowTestAdapter  extends RecyclerView.Adapter<ShowTestAdapter.TestV
     public TestViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Context context = parent.getContext();
         int layoutId=R.layout.test_list_item;
-
         LayoutInflater inflater = LayoutInflater.from(context);
         View view =inflater.inflate(layoutId,parent,false);
         TestViewHolder viewHolder = new TestViewHolder(view);
-       // viewHolder.viewHolderIndex.setText("Holder count"+String.valueOf(viewHolderCount)+"  "+test_names.get(viewHolderCount));
         viewHolderCount++;
         return viewHolder;
     }
@@ -51,11 +49,19 @@ public class ShowTestAdapter  extends RecyclerView.Adapter<ShowTestAdapter.TestV
     class TestViewHolder extends RecyclerView.ViewHolder {
 
         TextView viewHolderIndex;
-        TextView listItemTestView;
         public TestViewHolder(View itemView) {
             super(itemView);
             viewHolderIndex = itemView.findViewById(R.id.tv_view_holder_number);
-            listItemTestView = itemView.findViewById(R.id.tv_number_item);
+            viewHolderIndex.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                        //move to fragment with all shiit
+                    int positionIndex = getAdapterPosition(); //get element what we are just clicked
+                    viewHolderIndex.setText("I was chosen!");
+
+                }
+              }
+            );
         }
         void bind(int listIndex) {
             //listItemTestView.setText(test_names.get(listIndex));
