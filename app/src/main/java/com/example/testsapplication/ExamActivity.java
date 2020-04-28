1920bcd4587671711c10d1bc122d97f4eb306aee
CODER591 <coder591@loves_you.com>
2020-04-28 23:02:29 +0300

    Attempt to replace activity with fragment

--- app/src/main/java/com/example/testsapplication/ExamActivity.java
+++ app/src/main/java/com/example/testsapplication/ExamActivity.java
@@ -1,11 +1,12 @@
 package com.example.testsapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.example.testsapplication.testmanager.TestManager;
@@ -28,6 +29,13 @@ public class ExamActivity extends AppCompatActivity {
         start_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //This is not working at all, for some reason
+                Fragment questionFragment = new QuestionFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.add(R.id.fragment_container, questionFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
 
                 // start test
                 // start timer
