0cbdeb4ed68a779f203fe43522bbf4819fe4ba84
CODER591 <coder591@loves_you.com>
2020-04-30 19:53:15 +0300

    Added moving to MainActivity after ending with test

--- app/src/main/java/com/example/testsapplication/FinishPageFragment.java
+++ app/src/main/java/com/example/testsapplication/FinishPageFragment.java
@@ -1,12 +1,15 @@
 package com.example.testsapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.testsapplication.testmanager.TestManager;
@@ -36,10 +39,18 @@ public class FinishPageFragment extends Fragment {
         // Inflate the layout for this fragment
         View view =inflater.inflate(R.layout.fragment_finish_page, container, false);
         TextView tv_right_ans = view.findViewById(R.id.right_answers);
+        Button fin_btn=view.findViewById(R.id.finish_button);
         int all_qst_quantity = TestManager.getInstance().getCurrentExam().getExamQuestionsCount();
         int right_ans_quantity = TestManager.getInstance().getCurrentExam().getCorrectAnswers();
         tv_right_ans.setText(String.valueOf(right_ans_quantity)+"/"+String.valueOf(all_qst_quantity));
-        
+        fin_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
         return view;
     }
 }
